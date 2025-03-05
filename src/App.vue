45bcfe7db56c10c6ff48bce57c40ba731f90e5f5
turner <turneratc@163.com>
2025-03-05 22:05:13 +0800

    初始化ai-chat-app项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class=" text-xs bg-green-500">💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
