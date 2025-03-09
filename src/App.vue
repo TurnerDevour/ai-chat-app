2c625a58bca841a5462e50366fcf02639cb2472e
turner <turneratc@163.com>
2025-03-09 16:25:38 +0800

    添加 ProviderSelect 和 MessageInput 组件，更新类型定义，修复 createAt 为 createdAt，新增 radix-vue 依赖

--- src/App.vue
+++ src/App.vue
@@ -17,27 +17,61 @@
         </button>
       </div>
     </div>
-    <div class="h-full flex-1">
-      content
+    <div class="h-full flex-1 flex items-center">
+      <div class="w-[80%] mx-auto h-full">
+        <div class="flex items-center h-[85%]">
+          <ProviderSelect :items="providers" v-model="selectedModel" />
+        </div>
+        <div class="flex items-center h-[15%]">
+          <MessageInput />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
-import { ConversationProps } from './types'
+import { ConversationProps, ProviderProps } from './types'
 import ConversationList from './components/ConversationList.vue';
+import ProviderSelect from './components/ProviderSelect.vue';
+import MessageInput from './components/MessageInput.vue';
+import { ref } from 'vue';
+
+const selectedModel = ref<string>('')
 
 const items: ConversationProps[] = [
-  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 5, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 6, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 7, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 8, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
-  { id: 9, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 5, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 6, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 7, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 8, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+  { id: 9, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用？', createdAt: '2025-3-6', updatedAt: '2025,3-6', providerId: 12342 },
+]
+
+const providers: ProviderProps[] = [
+  {
+    id: 1,
+    name: '文心一言',
+    desc: '文心一言 百度出品的大模型',
+    models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
+    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    createdAt: '2024-07-03',
+    updatedAt: '2024-07-03'
+  },
+  {
+    id: 2,
+    name: '通义千问',
+    desc: '通义千问',
+    // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.0.5bf41507xgULX5#b148acc634pfc
+    models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
+    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    createdAt: '2024-07-03',
+    updatedAt: '2024-07-03'
+  }
 ]
 </script>
 
